eadf29eab4e64b383a5dd867412594bbbb3d1266
Amanda Theisen <amandatheisens+github@gmail.com>
2025-01-07 23:01:36 +0000

    add calculate submit result

--- script.js
+++ script.js
@@ -16,9 +16,31 @@ const PALETTE = {
     ACTIVE_BTN: "white",
 };
 
+const maleFemale = document.getElementById("male-female-id")
 const maleButton = document.getElementById("male-btn");
 const femaleButton = document.getElementById("female-btn");
-const ageInput = document.getElementById("age-input");
+const ageInput = document.getElementById("a-input");
+const heightInput = document.getElementById("h-input")
+const weightInput = document.getElementById("w-input")
+const weatherSelect = document.getElementById("weather-id")
+const activityLevelSelect = document.getElementById("activity-level-id")
+const mySubmit = document.getElementById("my-submit-form")
+
+let userGenderFactor = 0;
+
+maleFemale.addEventListener("click", (e) => {
+    const userGender = e.target.value
+    userGenderFactor;
+
+    if (userGender === "male") {
+        userGenderFactor = 500
+    } else {
+        userGenderFactor = 0
+    }
+
+    console.log(userGenderFactor)
+})
+
 
 function maleBtnClickHandler() {
     buttonsState.male = !buttonsState.male;
@@ -49,10 +71,123 @@ function swicthBtnState(btnType) {
     }
 }
 
-let genderFactor = 0;
 
-maleButton.addEventListener("click", maleBtnClickHandler)
 
-femaleButton.addEventListener("click", femaleBtnClickHandler)
+// maleButton.addEventListener("click", maleBtnClickHandler)
 
-ageInput.addEventListener("input", (e) => console.log(e.target.value))
+// femaleButton.addEventListener("click", femaleBtnClickHandler)
+
+// ageInput.addEventListener("input", (e) => console.log(e.target.value))
+
+
+let userAge = 0;
+let userAgeFactor = 0;
+
+ageInput.addEventListener("input", (e) => { 
+    userAge = e.target.value
+    userAgeFactor = calculateAgeFactor(userAge)
+    console.log(userAgeFactor)
+})
+
+function calculateAgeFactor(age) {
+    if (age < 30) {
+        return 0
+    } 
+    if (age <= 50) {
+        return -200
+    }
+    return -500
+}
+
+
+// let userHeight = 0;
+// let heightFactor = 0;
+
+let userHeightFactor = 0;
+
+heightInput.addEventListener("input", (e) => {
+    userHeight = e.target.value;
+    userHeightFactor;
+
+    if (userHeight >= 180 && userHeight < 190) {
+        userHeightFactor = 200
+    } else if (userHeight >= 190 && userHeight <= 210) {
+        userHeightFactor = 300
+    } else {
+        userHeightFactor = 0
+    }
+    
+    console.log(userHeightFactor)
+})
+
+// function calculateHeightFactor(height) {
+//     if(height >= 130 && height < 190) {
+//         return 200
+//     }
+//     if (height > 190 && height <= 210)
+//         return 300
+// }
+
+let userWeight = 0;
+let userWeightFactor = 0;
+
+weightInput.addEventListener("input", (e) => {
+    userWeight = e.target.value;
+    userWeightFactor = calculateWeightFactor(userWeight)
+    console.log(userWeightFactor)
+})
+
+function calculateWeightFactor(weight) {
+    let weightResult = weight * 35;
+    return weightResult
+}
+
+let userWeatherFactor = 0;
+
+weatherSelect.addEventListener("change", (e) => {
+    const userWeather = e.target.value
+    userWeatherFactor;
+
+    if (userWeather === "cold") {
+        userWeatherFactor = 200;
+    } else if (userWeather === "normal") {
+        userWeatherFactor = 0;
+    } else { 
+        userWeatherFactor = 500;
+    }
+
+    console.log(userWeatherFactor) 
+})
+
+let userActivityLevelFactor = 0;
+
+activityLevelSelect.addEventListener("change", (e) => {
+    const userActivityLevel = e.target.value
+    userActivityLevelFactor;
+
+    if (userActivityLevel === "sedentary") {
+        userActivityLevelFactor = 0
+    } else if (userActivityLevel === "moderate") {
+        userActivityLevelFactor = 400
+    } else {
+        userActivityLevelFactor = 700
+    }
+
+    console.log(userActivityLevelFactor)
+})
+
+let userSubmitResult = 0;
+
+mySubmit.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+        userSubmitResult = 
+        userWeightFactor +
+        userGenderFactor +
+        userHeightFactor +
+        userAgeFactor +
+        userWeatherFactor +
+        userActivityLevelFactor
+
+    console.log("Water intake result:", userSubmitResult)
+})
